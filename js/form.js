d0d72e425709c7ba00ad9760abbc48d55740a70c
willian.bueno <willianbueno@outlook.com>
2018-09-26 17:41:31 -0300

    Validações do formulario

--- js/form.js
+++ js/form.js
@@ -1,29 +1,81 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
-botaoAdicionar.addEventListener("click", function(event) {
+botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(montaTr(obtemPaciente(form)));
+    var paciente = obtemPaciente(form);
+
+    if (isPacienteValido(paciente)) {
+        var tabela = document.querySelector("#tabela-pacientes");
+        tabela.appendChild(montaTr(paciente));
+    }
 
 
 });
 
+function isPacienteValido(paciente) {
+    limpaMensagemError()
+    var erros = [];
+
+    erros.push(
+        validaNome(paciente.nome),
+        validaPeso(paciente.peso),
+        validaAltura(paciente.altura),
+        validaGordura(paciente.gordura)
+    )
+
+    erros.forEach(erro => {
+        if(erro){
+            document.querySelector(erro).textContent = erro.substring(String(erro).indexOf('-')+1)+" Invalido";
+        }
+    });
+    return !erros.some(erro => erro);
+}
+
+function validaNome(nome) {
+    if (nome.length <= 0)
+        return "#erro-nome";
+}
+
+function validaPeso(peso) {
+    if (peso <= 0)
+        return "#erro-peso";
+}
+
+function validaAltura(altura) {
+    if (altura <= 0)
+        return "#erro-altura";
+}
+
+function validaGordura(gordura) {
+    if (gordura <= 0)
+        return "#erro-gordura";
+
+}
+
+function limpaMensagemError() {
+    console.log(document.querySelectorAll("form span"));
+    document.querySelectorAll("span").forEach(span => {
+        span.textContent = "";
+    });
+}
+
+
 /**
  * Cria um Objeto que representa um paciente com base nos dados do formulario
- * @param {form} form 
+ * @param {form} form
  */
-function obtemPaciente(form){
+function obtemPaciente(form) {
     return {
-        nome : form.nome.value,
-        peso : form.peso.value,
-        altura : form.altura.value,
-        gordura : form.gordura.value,
-        imc : calculaImc(form.peso.value, form.altura.value)
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
     }
 }
 
-function montaTr(paciente){
+function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
 
     pacienteTr.classList.add("paciente");
@@ -36,7 +88,7 @@ function montaTr(paciente){
     tds.push(montaTd(paciente.gordura, "info-gordura"));
     tds.push(montaTd(paciente.imc, "info-imc"));
 
-    for (var x = 0 ; x < tds.length ; x++){
+    for (var x = 0; x < tds.length; x++) {
         pacienteTr.appendChild(tds[x]);
     }
 
